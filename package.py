a70445e8533228e2f57cfb29120ba01674e9cb70
Nick Thorp <nickthorp20@gmail.com>
2018-10-20 22:23:23 -0500

    More changes to building automash

--- package.py
+++ package.py
@@ -1,11 +1,14 @@
+import os
 import vdist.builder as builder
 from vdist.configuration import Configuration
 from vdist.source import directory
 
+project_dir = os.getcwd()
+
 builder_parameters = {
             "app": 'thebideo-comments',
             "version": '1.0',
-            "source": directory(path='C:\\Users\\nthor\source\\thebideo-comments'),
+            "source": directory(path=project_dir),
             "profile": 'centos7'
         }
 
