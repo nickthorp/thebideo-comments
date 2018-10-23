a6fb340a7abb228376b83020a69a46299e47a859
Nick Thorp <nickthorp20@gmail.com>
2018-10-22 19:42:22 -0500

    The Goof Troupe

--- package.py
+++ package.py
@@ -1,15 +1,17 @@
 import os
 import vdist.builder as builder
 from vdist.configuration import Configuration
-from vdist.source import directory
+from vdist.source import directory, git
 
 project_dir = os.getcwd()
 
 builder_parameters = {
             "app": 'thebideo-comments',
             "version": '1.0',
-            "source": directory(path=project_dir),
-            "profile": 'ubuntu-trusty',
+#            "source": directory(path=project_dir),
+            "source": git(uri='https://github.com/nickthorp/thebideo-comments',
+                          branch='master'),
+            "profile": 'centos7',
             "python_version": '3.6.6',
             "output_folder": '/tmp/vdist/'
         }
