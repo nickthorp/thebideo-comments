6b4b0ebebc726951e42f29947b3cf91d0e119c07
Nick Thorp <nickthorp20@gmail.com>
2018-10-25 20:43:35 -0500

    This is the money commit

--- package.py
+++ package.py
@@ -1,20 +1,21 @@
 import os
 import vdist.builder as builder
 from vdist.configuration import Configuration
-from vdist.source import directory, git
+from vdist.source import directory
 
+app = 'thebideo-comments'
 project_dir = os.getcwd()
+root_dir, tmp = os.path.split(project_dir)
+ln_dir = root_dir + '/' + app
+os.symlink(project_dir, ln_dir, True)
 
 builder_parameters = {
-            "app": 'thebideo-comments',
+            "app": app,
             "version": '1.0',
-            "source": directory(path=project_dir),
-#            "source": git(uri='https://github.com/nickthorp/thebideo-comments',
-#                          branch='master'),
+            "source": directory(path=ln_dir),
             "profile": 'centos7',
             "python_version": '3.6.6',
             "requirements_path": '/requirements.txt',
-
         }
 
 configuration = Configuration(builder_parameters)
